a41a82f8463eb3f313ed7afaeb16bee05b10947f
Juliana Al-Ola <alolajuliana@gmail.com>
2024-11-01 14:09:54 -0400

    adjusting sound

--- 2d-game-project/game_menu_sound.gd
+++ 2d-game-project/game_menu_sound.gd
@@ -1,7 +1,5 @@
 extends Node2D
-@onready var audio = $buttonClick
-
 
 func _on_startbutton_pressed():
-	audio.play()
+	$buttonClick.play()
 	get_tree().change_scene_to_file("res://starry_sky.tscn")
