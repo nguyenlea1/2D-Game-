a0f7ee051a592acfbeeeb97ceed5b8a592a52dbf
Juliana Al-Ola <alolajuliana@gmail.com>
2024-11-01 11:28:51 -0400

    increased the game difficulty as time increases

--- 2d-game-project/game_menu_sound.gd
+++ 2d-game-project/game_menu_sound.gd
@@ -1,6 +1,7 @@
 extends Node2D
 @onready var audio = $buttonClick
 
-func _on_enter_store_button_pressed():
+
+func _on_startbutton_pressed():
 	audio.play()
 	get_tree().change_scene_to_file("res://starry_sky.tscn")
